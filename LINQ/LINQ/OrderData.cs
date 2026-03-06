405afca5e21c57360f140aeb0d228c4ad86a2c0b
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-03-06 09:52:07 +0200

    OrderData

--- LINQ/LINQ/OrderData.cs
+++ LINQ/LINQ/OrderData.cs
@@ -2,5 +2,80 @@
 {
     public class OrderData
     {
+        public static readonly List<Order> order = new List<Order>
+        {
+                new Order()
+                {
+                    Id = 1,
+                    ClientId = 1,
+                    ProductId = 6,
+                    Amount = 3
+                },
+                new Order()
+                {
+                    Id = 2,
+                    ClientId = 3,
+                    ProductId = 4,
+                    Amount = 5
+                },
+                new Order()
+                {
+                    Id = 3,
+                    ClientId = 5,
+                    ProductId = 2,
+                    Amount = 2
+                },
+                new Order()
+                {
+                    Id = 4,
+                    ClientId = 2,
+                    ProductId = 1,
+                    Amount = 10
+                },
+                new Order()
+                {
+                    Id = 5,
+                    ClientId = 1,
+                    ProductId = 9,
+                    Amount = 20
+                },
+                new Order()
+                {
+                    Id = 6,
+                    ClientId = 4,
+                    ProductId = 10,
+                    Amount = 5
+                },
+                new Order()
+                {
+                    Id = 7,
+                    ClientId = 3,
+                    ProductId = 5,
+                    Amount = 3
+                },
+                new Order()
+                {
+                    Id = 8,
+                    ClientId = 5,
+                    ProductId = 1,
+                    Amount = 5
+                },
+                new Order()
+                {
+                    Id = 9,
+                    ClientId = 1,
+                    ProductId = 3,
+                    Amount = 10
+                },
+                new Order()
+                {
+                    Id = 10,
+                    ClientId = 2,
+                    ProductId = 7,
+                    Amount = 15
+                }
+            };
+        }
     }
-}
+    
+
