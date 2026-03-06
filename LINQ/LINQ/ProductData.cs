ed499ff9b07b9e3c0c07a749c17df823dce823c4
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-03-06 09:39:59 +0200

    ProductData

--- LINQ/LINQ/ProductData.cs
+++ LINQ/LINQ/ProductData.cs
@@ -7,35 +7,35 @@
             new Product()
             {
                 Id = 1,
-                Name = "Mia",
-                Category = "Tallinn",
-                Price = 
-                Amount =
+                Name = "Car",
+                Category = "Vehicle",
+                Price = 5589,
+                Amount = 30
             },
             new Product()
             {
                 Id = 2,
-                Name = "Moona",
-                Category = "Haapsalu",
-                Price =
-                Amount =
+                Name = "Cheese",
+                Category = "Dairy",
+                Price = 4.35,
+                Amount = 20
 
             },
             new Product()
             {
                 Id = 3,
-                Name = "Janar",
-                Category = "Rakvere",
-                Price =
-                Amount =
+                Name = "Hat",
+                Category = "Clothes",
+                Price = 6.89,
+                Amount = 100
             },
             new Product()
             {
                 Id = 4,
-                Name = "Maiduk",
-                Category = "Tallinn",
-                Price =
-                Amount =
+                Name = "Basketball",
+                Category = "Sport",
+                Price = 11.99,
+                Amount = 25
             },
             new Product()
             {
@@ -44,6 +44,46 @@
                 Category = "Dairy",
                 Price = 2.45,
                 Amount = 10
+            },
+            new Product()
+            {
+                Id = 6,
+                Name = "Sneaker",
+                Category = "Shoe",
+                Price = 101.99,
+                Amount = 50
+            },
+            new Product()
+            {
+                Id = 7,
+                Name = "Flip Flops",
+                Category = "Shoe",
+                Price = 55.89,
+                Amount = 35
+            },
+            new Product()
+            {
+                Id = 8,
+                Name = "Sausage",
+                Category = "Meat",
+                Price = 3.99,
+                Amount = 20
+            },
+            new Product()
+            {
+                Id = 9,
+                Name = "Yogurt",
+                Category = "Dairy",
+                Price = 2.99,
+                Amount = 25
+            },
+            new Product()
+            {
+                Id = 10,
+                Name = "Sandals",
+                Category = "Shoe",
+                Price = 45.89,
+                Amount = 45
             }
         };
     }
