972ac9b5ce102589d0172745c59a61f271a9310e
TTVHardCoreGamer <aaronvork22@gmail.com>
2026-03-06 09:34:06 +0200

    ClientDataAndProductData

--- LINQ/LINQ/ClientData.cs
+++ LINQ/LINQ/ClientData.cs
@@ -2,5 +2,42 @@
 {
     public class ClientData
     {
+        public static readonly List<Client> client = new List<Client>
+        {
+                new Client()
+                {
+                    Id = 1,
+                    Name = "Mia",
+                    City = "Tallinn"
+                },
+                new Client()
+                {
+                    Id = 2,
+                    Name = "Moona",
+                    City = "Haapsalu"
+                },
+                new Client()
+                {
+                    Id = 3,
+                    Name = "Janar",
+                    City = "Rakvere"
+                },
+                new Client()
+                {
+                    Id = 4,
+                    Name = "Maiduk",
+                    City = "Tallinn"
+                },
+                new Client()
+                {
+                    Id = 5,
+                    Name = "Targo",
+                    City = "Tartu"
+                }
+            };
+        }
     }
-}
+
+
+
+             
--- LINQ/LINQ/ProductData.cs
+++ LINQ/LINQ/ProductData.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LINQ
+﻿namespace LINQ
 {
-    class ProductData
+    public class ProductData
     {
+        public static readonly List<Product> product = new List<Product>
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Mia",
+                Category = "Tallinn",
+                Price = 
+                Amount =
+            },
+            new Product()
+            {
+                Id = 2,
+                Name = "Moona",
+                Category = "Haapsalu",
+                Price =
+                Amount =
+
+            },
+            new Product()
+            {
+                Id = 3,
+                Name = "Janar",
+                Category = "Rakvere",
+                Price =
+                Amount =
+            },
+            new Product()
+            {
+                Id = 4,
+                Name = "Maiduk",
+                Category = "Tallinn",
+                Price =
+                Amount =
+            },
+            new Product()
+            {
+                Id = 5,
+                Name = "Milk",
+                Category = "Dairy",
+                Price = 2.45,
+                Amount = 10
+            }
+        };
     }
 }
